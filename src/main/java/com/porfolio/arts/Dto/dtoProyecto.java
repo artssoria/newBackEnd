7a9e04f34f60e4979d8c88cb6a1181b799470de9
artssoria <53129227+artssoria@users.noreply.github.com>
2022-07-31 03:17:57 -0300

    arreglos

--- src/main/java/com/porfolio/arts/Dto/dtoProyecto.java
+++ src/main/java/com/porfolio/arts/Dto/dtoProyecto.java
@@ -8,7 +8,7 @@ package com.porfolio.arts.Dto;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.porfolio.arts.Dto;
+
 
 
 import javax.validation.constraints.NotBlank;
