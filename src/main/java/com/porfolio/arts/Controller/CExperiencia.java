3d5f99fe06d87a6da88a3a10bb6ece1bb2bc8617
artssoria <53129227+artssoria@users.noreply.github.com>
2022-07-31 05:52:21 -0300

    Update CExperiencia.java

--- src/main/java/com/porfolio/arts/Controller/CExperiencia.java
+++ src/main/java/com/porfolio/arts/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://frontendarg-2b995.web.app/")
+@CrossOrigin(origins = "https://frontendarg-2b995.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
