6f5b60bb9bf964ab4e31f2200470d4b96b7a7e69
artssoria <53129227+artssoria@users.noreply.github.com>
2022-07-31 20:31:46 -0300

    incorporacion de cors

--- src/main/java/com/porfolio/arts/ArtsApplication.java
+++ src/main/java/com/porfolio/arts/ArtsApplication.java
@@ -2,12 +2,24 @@ package com.porfolio.arts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ArtsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArtsApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+            }
+        };
+    }
 }
