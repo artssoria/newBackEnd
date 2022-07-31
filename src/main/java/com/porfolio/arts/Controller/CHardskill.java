4098e7f9ffb9eb7b50b207d0f77ea598fe986d6d
artssoria <53129227+artssoria@users.noreply.github.com>
2022-07-31 05:26:44 -0300

    Update CHardskill.java

--- src/main/java/com/porfolio/arts/Controller/CHardskill.java
+++ src/main/java/com/porfolio/arts/Controller/CHardskill.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hardskill")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendarg-2b995.web.app/")
 public class CHardskill {
     @Autowired
     SHardskill sHardskill;
