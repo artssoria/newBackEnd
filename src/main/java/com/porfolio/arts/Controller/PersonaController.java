298788e06821776f2da9ac0beee0772a2650d401
artssoria <53129227+artssoria@users.noreply.github.com>
2022-08-02 23:14:25 -0300

    arreglando las rutas

--- src/main/java/com/porfolio/arts/Controller/PersonaController.java
+++ src/main/java/com/porfolio/arts/Controller/PersonaController.java
@@ -13,13 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/personas")
-@CrossOrigin(origins = "https://frontendarg-2b995.web.app")
+
+@CrossOrigin(origins = "https://frontendarg-2b995.web.app/personas/")
 
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
